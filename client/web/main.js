7c0a1291cbc1972cef2ed8bc6687812a1c3c20c6
troido <troido@protonmail.com>
2022-11-16 17:56:07 +0100

    send chat messages from web client

--- client/web/main.js
+++ client/web/main.js
@@ -19,7 +19,6 @@ function start(e) {
 	let client = new Client(username, host, canvas);
 	client.start()
 	form.hidden = true;
-	canvas.hidden = false;
 }
 
 
@@ -35,9 +34,42 @@ class Client {
 		console.log("connecting to '" + this.host + "' as '" + this.username + "'");
 		this.websocket = new WebSocket(this.host);
 		this.websocket.addEventListener("open", e => {
+			document.getElementById("game").hidden = false;
 			e.target.send(JSON.stringify({introduction: this.username}));
 		});
 		this.websocket.addEventListener("error", console.error);
-		this.websocket.addEventListener("message", console.log);
+		this.websocket.addEventListener("message", msg => this.handleMessage(msg));
+		document.getElementById("chat").addEventListener("submit", e => {
+			let inp = e.target.command;
+			this.onCommand(inp.value)
+			inp.value = "";
+		});
+	}
+	
+	handleMessage(msg) {
+		let data = JSON.parse(msg.data)
+		let type = data[0];
+		if (type === "message") {
+			this.print(data[1]);
+		} else if (type === "messages") {
+			for (let mesg of data[1]) {
+				this.print(data[1], data[0]);
+			}
+		} else if (type === "world") {
+			void(0);
+		} else {
+			console.log("unknown", data);
+		}
+	}
+	
+	print(msg, type) {
+		console.log("msg", msg);
+		let li = document.createElement("li");
+		li.innerText = msg;
+		document.getElementById("messages").appendChild(li);
+	}
+	
+	onCommand(command) {
+		this.websocket.send(JSON.stringify({chat: command}));
 	}
 }
